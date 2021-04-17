4b27e3793c9216798d3df0f2f9694bc20447cc1a
Priyanka Chaudhary <priyanka.chaudhary18@gmail.com>
2021-04-17 20:39:26 +0200

    Impartial Offerings solution

---
+++ code_jam_women_2021/Impartial Offerings.py
@@ -0,0 +1,21 @@
+# your code goes here
+def cal_treats(pet_s):
+	treat_size = 1
+	treat_tot = 1
+	for i in range(1, len(pet_s)):
+		if pet_s[i] == pet_s[i-1]:
+			treat_tot = treat_tot + treat_size
+		else:
+			treat_size = treat_size + 1
+			treat_tot = treat_tot + treat_size
+	return treat_tot
+
+num_cases = int(input())
+for case in range(1, num_cases + 1):
+	num_pets = int(input())
+	size_pets = input().split()
+	size_pets = list(map(int, size_pets))
+	size_pets.sort()
+	result = cal_treats(size_pets)
+	#print(size_pets)
+	print('Case #{}: {}'.format(case, result))
